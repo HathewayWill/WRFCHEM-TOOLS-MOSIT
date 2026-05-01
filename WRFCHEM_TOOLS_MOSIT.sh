0ab1e0aa9f493ff0564a904712789630f9924632
HathewayWill <118180342+HathewayWill@users.noreply.github.com>
2026-04-30 21:26:34 -0500

    Add files via upload
    fixed intel oneapi compiler new file names and paths

--- WRFCHEM_TOOLS_MOSIT.sh
+++ WRFCHEM_TOOLS_MOSIT.sh
@@ -3216,6 +3216,7 @@ if [ "$Ubuntu_64bit_Intel" = "1" ]; then
   echo "$PASSWD" | sudo -S apt -y install bison build-essential byacc cmake csh curl default-jdk default-jre flex libfl-dev g++ gawk gcc gettext gfortran git ksh libcurl4-gnutls-dev libjpeg-dev libncurses6 libncursesw5-dev libpixman-1-dev libpng-dev libtool libxml2 libxml2-dev libxml-libxml-perl m4 make ncview pipenv pkg-config python3 python3-dev python3-pip python3-dateutil tcsh unzip xauth xorg time ghostscript less libbz2-dev libc6-dev libffi-dev libgdbm-dev libopenblas-dev libreadline-dev libssl-dev libtiff-dev libgeotiff-dev tk-dev vim wget
 
   # install the Intel compilers
+  echo $PASSWD | sudo -S apt -y install intel-oneapi-toolkit
   echo $PASSWD | sudo -S apt -y install intel-basekit
   echo $PASSWD | sudo -S apt -y install intel-hpckit
   echo $PASSWD | sudo -S apt -y install intel-oneapi-python
